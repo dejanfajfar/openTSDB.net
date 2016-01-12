1c3c84e24816e3fd9a47318d9195f3377f9f5763
Dejan FAJFAR <dejan@fajfar.com>
2016-01-12 16:03:22 +0100

    Fixing unix timestamp calculation

--- openTSDB.net/DateTimeExtentions.cs
+++ openTSDB.net/DateTimeExtentions.cs
@@ -6,7 +6,7 @@ namespace openTsdbNet
     {
         public static int ToUnixEpoch(this DateTime dateTime)
         {
-            var timeSinceDisco = dateTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var timeSinceDisco = TimeZoneInfo.ConvertTimeToUtc(dateTime.ToUniversalTime()) - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             return (int) timeSinceDisco.TotalSeconds;
         }
     }
